9358d9efa5df46c6409dcc69bd35d3aa7d540b09
Bolo <Chengdu@Bolos-Mac-mini.local>
2017-06-08 10:28:29 +0800

    更新进度条使用方法

--- 进度条/UserLevelProgressView.h
+++ 进度条/UserLevelProgressView.h
@@ -10,8 +10,20 @@
 
 @interface UserLevelProgressView : UIView
 
+/**
+ - (UserLevelProgressView *)progressView{
+ if(!_progressView){
+ _progressView = [[UserLevelProgressView alloc]initWithFrame:BQAdaptationFrame(self.labelExp.zlMaxX+10, self.labelExp.zlMinY, IPHONE_WIDTH - 260, 30)];
+ [_progressView setProgressWidth:IPHONE_WIDTH - 260 height:30];
+ _progressView.progress = 0.75;
+ _progressView.labelTitle = @"";
+ }
+ return _progressView;
+ }
+ 
+ 首先调用设置进度条宽高。然后再设置值
+ */
 @property (nonatomic,strong) NSString *labelTitle;//eg:"300/600"
-//注意。设置进度应该在调用setProgressWith之后来设置进度
 @property (nonatomic,assign) CGFloat progress;
 - (void) setProgressWidth:(CGFloat) width height:(CGFloat) height;//设置进度条的宽高
 @end
--- 进度条/UserLevelProgressView.m
+++ 进度条/UserLevelProgressView.m
@@ -13,6 +13,8 @@
 
 @property (nonatomic,strong) UIView *backView;
 @property (nonatomic,strong) UILabel *label;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
 @end
 @implementation UserLevelProgressView
 
@@ -24,7 +26,7 @@
         self.layer.masksToBounds = YES;
         [self addSubview:self.backView];
         [self addSubview:self.label];
-        self.backgroundColor = [UIColor grayColor];
+        self.backgroundColor = MY_COLOR(244, 244, 244, 1.0);
     }
     return self;
 }
@@ -33,6 +35,8 @@
     self.backView.frame = BQAdaptationFrame(-width, 0, width, height);
     self.label.bounds = BQAdaptationFrame(0, 0, width, height);
     self.label.center = BQadaptationCenter(CGPointMake(width/2, height/2));
+    self.width = width;
+    self.height = height;
 }
 - (void)setLabelTitle:(NSString *)labelTitle{
     _labelTitle = labelTitle;
@@ -41,7 +45,7 @@
 - (UIView *)backView{
     if(!_backView){
         _backView = [[UIView alloc]initWithFrame:BQAdaptationFrame(-350, 0, 350, 20)];
-        _backView.backgroundColor = MY_COLOR(252, 73, 128, 1.0);
+        _backView.backgroundColor = MainRedColor;
     }
     return _backView;
 }
@@ -51,7 +55,7 @@
         _label.bounds = BQAdaptationFrame(0, 0, 200, 20);
         _label.center = BQadaptationCenter(CGPointMake(350/2, 20/2));
         _label.text = @"1922/121345";
-        _label.font = [UIFont systemFontOfSize:25*BQAdaptationWidth()];
+        _label.font = [UIFont systemFontOfSize:fontMid()];
         _label.textAlignment = NSTextAlignmentCenter;
     }
     return _label;
@@ -59,6 +63,7 @@
 - (void)setProgress:(CGFloat)progress{
     _progress = progress;
     //设置进度条位置
+    self.backView.frame = BQAdaptationFrame(-self.width, 0, self.width, self.height);
     CGRect frame = self.backView.frame;
     frame.origin.x = progress * frame.size.width + frame.origin.x;
     self.backView.frame = frame;
